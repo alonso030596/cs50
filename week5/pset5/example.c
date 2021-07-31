c40ee2e2b9dc7bd42d1c1d8833a7d6045ba334cc
alonso030596 <alonso_030596@hotmail.com>
2021-07-31 00:21:34 +0000

    speller.c added

---
+++ week5/pset5/example.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int n = pow(26, 3);
+
+    printf("%d\n", n);
+}
--- week5/pset5/speller/dictionary.c
+++ week5/pset5/speller/dictionary.c
@@ -12,8 +12,14 @@ typedef struct node
 }
 node;
 
+// Constants
+#define ALPHABET_LENGTH 26
+
+// Number of letters in each bucket ! CHANGE !
+#define LETTERS_PER_BUCKET 2
+
 // Number of buckets in hash table
-const unsigned int N = 1;
+const unsigned int N = pow(ALPHABET_LENGTH, LETTERS_PER_BUCKET);
 
 // Hash table
 node *table[N];
@@ -28,15 +34,21 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO
+
     return 0;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    FILE *fptr = fopen(dictionary, "r");
+
+    if(ptr == NULL)
+        return false;
+
+    while(fgets(, , dictionary) != EOF)
+
+
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
